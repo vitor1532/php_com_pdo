75e817e66ecc0d874278bc98952c7b681b6fe1f8
unknown <vitomartinsalmeida@gmail.com>
2022-01-14 15:59:11 -0300

    criando a query

--- index.php
+++ index.php
@@ -9,9 +9,30 @@
 	$usuario = 'root';
 	$senha = '';
 
-	$conexao = new PDO($dsn, $usuario, $senha);
+	
 
+	try {
+		$conexao = new PDO($dsn, $usuario, $senha);
 
+		$query = '
+			create table if not exists tb_usuarios(
+				id int not null primay key auto_increment,
+				nome varchar(50) not null,
+				email varchar(100) not null,
+				senha varchar(32) not null
+			)
+		';
+		//CRUD - C(create), R(read), U(update), D(delete)
+		//retorna a quantidade de linhas modificadas, criadas ou modificadas no bd, resultando geralmente em 0
+		$conexao->exec($query);
+		//0
+
+	} catch(PDOException $e) {
+		//recuperar CODE e MESSAGE
+		//CODE
+		echo 'Erro: '.$e->getCode().' Mensagem:'.$e->getMessage();
+		//final public string Exception::getMessage
+	}
 
 
 ?>
